8306b71cb4f38909a5170e5c9fca016e1c7564d3
Kps-96 <kpsitmail007@gmail.com>
2025-05-12 18:10:19 +0530

    Added the Custom JavaScript Dropdown

---
+++ drop.js
@@ -0,0 +1,112 @@
+function drop1(){
+    const ans = document.getElementById("answer1");
+    const question = document.getElementById("question1");
+    const img = document.getElementById("img1");
+
+    if(ans.style.display === "block"){
+        ans.style.display = "none";
+        question.classList.remove("active");
+        img.src = "./src/img/bottom.png";
+    } else {
+        ans.style.display = "block";
+        question.classList.toggle("active");
+        img.src = "./src/img/up.png";
+    }
+}
+
+
+function drop2(){
+    const ans = document.getElementById("answer2");
+    const question = document.getElementById("question2");
+    const img = document.getElementById("img2");
+
+    if(ans.style.display === "block"){
+        ans.style.display = "none";
+        question.classList.remove("active");
+        img.src = "./src/img/bottom.png";
+    } else {
+        ans.style.display = "block";
+        question.classList.toggle("active");
+        img.src = "./src/img/up.png";
+    }
+}
+
+function drop3(){
+    const ans = document.getElementById("answer3");
+    const question = document.getElementById("question3");
+    const img = document.getElementById("img3");
+
+    if(ans.style.display === "block"){
+        ans.style.display = "none";
+        question.classList.remove("active");
+        img.src = "./src/img/bottom.png";
+    } else {
+        ans.style.display = "block";
+        question.classList.toggle("active");
+        img.src = "./src/img/up.png";
+    }
+}
+
+function drop4(){
+    const ans = document.getElementById("answer4");
+    const question = document.getElementById("question4");
+    const img = document.getElementById("img4");
+
+    if(ans.style.display === "block"){
+        ans.style.display = "none";
+        question.classList.remove("active");
+        img.src = "./src/img/bottom.png";
+    } else {
+        ans.style.display = "block";
+        question.classList.toggle("active");
+        img.src = "./src/img/up.png";
+    }
+}
+
+function drop5(){
+    const ans = document.getElementById("answer5");
+    const question = document.getElementById("question5");
+    const img = document.getElementById("img5");
+
+    if(ans.style.display === "block"){
+        ans.style.display = "none";
+        question.classList.remove("active");
+        img.src = "./src/img/bottom.png";
+    } else {
+        ans.style.display = "block";
+        question.classList.toggle("active");
+        img.src = "./src/img/up.png";
+    }
+}
+
+function drop6(){
+    const ans = document.getElementById("answer6");
+    const question = document.getElementById("question6");
+    const img = document.getElementById("img6");
+
+    if(ans.style.display === "block"){
+        ans.style.display = "none";
+        question.classList.remove("active");
+        img.src = "./src/img/bottom.png";
+    } else {
+        ans.style.display = "block";
+        question.classList.toggle("active");
+        img.src = "./src/img/up.png";
+    }
+}
+
+function openAll(){
+    const all = document.getElementById("expand");
+    if(all.textContent === "EXPAND ALL"){
+        all.textContent = "CLOSE ALL";
+    } else {
+        all.textContent = "EXPAND ALL";
+    }
+
+    drop1();
+    drop2();
+    drop3();
+    drop4();
+    drop5();
+    drop6();
+}
